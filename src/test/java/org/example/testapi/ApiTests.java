5576943ad8ed73cb9499fd9c817867376905a026
brunovs80 <bruno.xd@live.com>
2020-11-12 17:54:35 -0300

    api

---
+++ src/test/java/org/example/testapi/ApiTests.java
@@ -0,0 +1,27 @@
+package org.example.testapi;
+
+import io.restassured.response.ValidatableResponse;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+
+public class ApiTests {
+
+    @BeforeClass
+    public static void setup() {
+        baseURI = "https://reqres.in";
+        basePath = "/api";
+    }
+
+    @Test
+    public void addUserSuccess() {
+        ValidatableResponse validatableResponse = given().log().all().
+                body("{\"name\": \"Bruno\", \"job\": \"QA\"}").
+                when().
+                post("/users").
+                then().
+                statusCode(201);
+
+    }
+
+}
